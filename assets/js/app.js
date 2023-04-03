c02b23104711c09a6cca52f5eeedbcc4deec9ae1
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-04-04 01:38:02 +0300

    3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,8 @@
 // localStorage.setItem("firstcarkey", "value");
 // console.log(localStorage.getItem("firstcar"));
 
-
 let  userInfoUser = document.querySelector(".user-info .user");
 let  userInfo = document.querySelector(".user-info");
-// let userdata = document.querySelector(".user-info .user");
 let links = document.querySelector("#links");
 
 if (localStorage.getItem("userName") ) {
@@ -17,14 +15,84 @@ if (localStorage.getItem("userName") ) {
     userInfoUser.style.color = "#9b916e";
 
     userInfoUser.innerHTML = "Welcome  " + localStorage.getItem( "userName");
+}
+// ---------------------------------------------------------------------------------------------
+let logout = document.querySelector(".logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+        window.location = "login.html";
+    }, 1200);
+});
+// ----------------------------------------------------------------------------------------------
+let allProducts = document.querySelector(".products");
+let proudcts = [
+    {
+        id: 1,
+        title: "car",
+        desc: "bmw",
+        color: "red",
+        img: "./img/coco.jpg "
+    },
+    {
+        id: 2,
+        title: "perfum",
+        desc: "perfun channel",
+        color: "blue",
+        img: "./img/car.jpeg "
+    },
+    {
+        id: 3,
+        title: "iphone",
+        desc: "ihpne 14 pro max",
+        color: "yellow",
+        img: "./img/iphon.jpg "
+    },
+] 
+function drowProducts() {
+    let y = proudcts.map((itm) => {
+        return ` 
+         <div class="product-item">
+                        <img src="${itm.img}" alt="coco">
+                        <div class="prod-item-des">
+                            <h2>${itm.title} </h2>
+                            <p>${itm.desc}</p>
+                            <span>${itm.color}</span>
+                        </div>  
+                        <div class="prod-item-action">
+                       <!-- /* //intercew Qstionnnnnnnnn// */ -->
+                            <!-- // <button onclick="check()"> add to card</button> -->
+                             <button class = "addToCard" onclick="addToCard(${itm.id})"> add to card</button>
+                            <i class="fa fa-heart"></i>
+                        </div>  
+         </div> 
+        `
+    });
+    allProducts.innerHTML = y;
+}
+drowProducts();
+//-----------------------------------------------------------------------------------------------
+
+function check() {
+    
+    if (localStorage.getItem("userName")) {
+        window.location = "cardProduct.html";
+    } else {
+        window.location = "login.html";
+    }
 
 }
+// --------------------------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------
+// let cardsProd = document.querySelector(".cards-prod");
+let cardsProdBox = document.querySelector(".cards-prod div");
 
+function addToCard(id) {
+    // console.log(id)
+    let choosItem = proudcts.find( (item) => item.id === id  );    
+        // console.log(choosItem)
+    
+    
+    cardsProdBox.innerHTML += `<p> ${choosItem.title} </p>  `;
 
-
-
-
-
-
+}
